38fe908d84e8118594d69179fab26eaf0d9b24d0
Dallas Yu <dallas_yu@hotmail.com>
2025-06-30 13:53:45 -0500

    done

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarLotSimulator;
+
+public class Car
+{
+    //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+    public int Year { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string EngineNoise { get; set; }
+    public string HonkNoise { get; set; }
+    public bool IsDriveable { get; set; }
+
+    public void MakeEngineNoise(string noise)
+    {
+        Console.WriteLine($"Engine noise: {noise}");
+    }
+
+    public void MakeHonkNoise(string noise)
+    {
+        Console.WriteLine($"Honk noise: {noise}");
+    }
+    
+    
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -17,7 +17,19 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Car car1 = new Car {Year = 2019, Make = "Toyota", Model = "Highlander", EngineNoise = "VROOM", HonkNoise = "BEEP", IsDriveable = true};
+            Car car2 = new Car {Year = 2020, Make = "Toyota", Model = "Camry", EngineNoise = "VROOM", HonkNoise = "BEEP", IsDriveable = true};
+            Car car3 = new Car {Year = 2021, Make = "Toyota", Model = "Corolla", EngineNoise = "VROOM", HonkNoise = "BEEP", IsDriveable = true};
 
+            car1.MakeEngineNoise("VROOM");
+            car1.MakeHonkNoise("BEEP");
+            
+            car2.MakeEngineNoise("VROOM");
+            car2.MakeHonkNoise("BEEP");
+            
+            car3.MakeEngineNoise("VROOM");
+            car3.MakeHonkNoise("BEEP");
+            
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
